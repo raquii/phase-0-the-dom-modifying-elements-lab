406809aac398beb1e323370ef3aebfd5a1c1dea0
raquel <raquelanaroman@gmail.com>
2021-05-03 17:10:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove();
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+
+const newContent = document.createTextNode("Raquel is the champion");
+    
+newHeader.appendChild(newContent);
+    
+document.body.appendChild(newHeader); 
